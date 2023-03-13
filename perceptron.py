fix wrong loss calculation

perceptron.py
@@ -1,9 +1,16 @@
 import numpy as np
+
+from random import random
 from typing import List
 from dataclasses import dataclass, field
 
 
-def step(value, threshold=0.5) -> bool:
+def random_points(n_points):
+    points = [(2 * random() - 1, 2 * random() - 1) for _ in range(n_points)]
+    targets = [int(point[0] > point[1]) for point in points]
+    return np.array(points), np.array(targets)
+
+def step(value, threshold=0.5) -> int:
     return 1 if value > threshold else 0
 
 @dataclass
@@ -13,21 +20,20 @@ class Perceptron:
     threshold: float = 0.1
     learning_rate: float = 0.1
     verbose:bool = False
-    weights: List[int] = field(init=False)
+    weights: np.ndarray = field(init=False)
 
     def __post_init__(self):
         self.weights = np.random.rand(1, self.n_inputs) 
 
-    def predict(self, input: List[float], target: bool) -> bool:
+    def predict(self, input: List[float], target: int) -> int:
         weighted_sum = np.dot(self.weights, input) + self.bias
         prediction = step(weighted_sum, self.threshold)
         if self.verbose:
             print(f"{weighted_sum=} | {prediction==target=}")
         return prediction
 
-    def fit(self, input, target) -> bool:
+    def fit(self, input, target) -> int:
         prediction = self.predict(input, target)
-        # self.bias = self.bias + self.learning_rate * (target - int(prediction)) * input
         for i, w in enumerate(self.weights):
             self.weights[i] = w +  self.learning_rate * (target - int(prediction)) * input
             if self.verbose:
@@ -39,29 +45,18 @@ class Perceptron:
         misses = 0
         for input, target in zip(inputs, targets):
             predicted = self.fit(input, target)
-            misses += int(predicted)
+            prediction_is_true = target == predicted
+            misses += 0 if prediction_is_true else 1
         if self.verbose:
             print(f"from {len(inputs)} set of inputs, the model missed {misses}.")
         return misses / len(inputs)
 
 
 def main():
-    x = np.array ([ [1, 1],
-                    [2, 2],
-                    [3, 3],
-                    [4, 4],
-                    [5, 5] ])
-    y = np.array([  [0],
-                    [0],
-                    [0],
-                    [0],
-                    [1] ])
-
-    p = Perceptron(verbose=True)
-    p.predict(x[0], y[0])
-    p.fit(x[0], y[0])
+    x, y = random_points(200)
+    p = Perceptron(verbose=False)
     loss = p.train(x, y)
-    print(loss)
+    print(f"your loss is: {loss}")
 
 if __name__ == "__main__":
     main()